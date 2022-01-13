48823510a1d046b7e07c767c114ab2d5d5ef1566
leiflinder <leif@leiflinder.com>
2022-01-13 15:41:48 -0500

    2022 01 13 B

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -4,25 +4,39 @@ import "../App.css";
 import { Table } from 'reactstrap';
 
 
-
 export const Roster = () => {
     return (
       <>
         <div className="roster-container">
-        <Table striped>
+        <Table striped className="table-responsive">
 		<tr>
-		<th>FN</th>
-		<th>LN</th>
-		<th>NM</th>
-        <th>POS</th>
+        <th>PIC</th>
+		<th>First</th>
+		<th>Last</th>
+		<th>Num</th>
+        <th>Pos</th>
+        <th>Born</th>
+        <th>HT</th>
+        <th>Weight</th>
+        <th>Year</th>
+        <th>TWC</th>
+        <th>HCC</th>
 		</tr>
           {ROSTER.map((data, key) => {
             return (
             <tr key={data.pid}>
+                {/* <td><img src="https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/'{data.pid}'.png"/></td> */}
+                <td><img src={'https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/'+data.pid+'.png'}/></td>
                 <td>{data.fn}</td>
                 <td>{data.ln}</td>
-                <td>{data.nm}</td>
-                <td>{data.pos}</td>   
+                <td>{data.num}</td>
+                <td>{data.pos}</td>
+                <td>{data.dob}</td> 
+                <td>{data.ht}</td> 
+                <td>{data.wt}</td> 
+                <td>{data.y}</td>
+                <td>{data.twc}</td>
+                <td className='HCC_TD'>{data.hcc}</td>        
             </tr>
             );
           })}
