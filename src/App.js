d29d15be04e5668668a408a21f5598b354f5be8a
leiflinder <leif@leiflinder.com>
2022-01-13 16:46:25 -0500

    2022 01 13 C

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import './App.css';
 // import ROSTER from './shared/nets_roster';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import {Roster} from './components/Roster';
-
+import logo from './bnets-logo.svg';
 function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <div className="App">
-        <Roster />
+return (
+<div>
+   <div className="App">
+      <div className="container">
+         <div className="navContainer">
+            <div className="logo_container">
+               <img src={logo} alt="logo" className="netslogo"/>
+            </div>
+            <div className="title_container">
+               <h1>Roster</h1>
+            </div>
+            <br className="clearLeft"/>
+         </div>
       </div>
-    </div>
-  );
+      <Roster />
+   </div>
+</div>
+);
 }
-
 export default App;
